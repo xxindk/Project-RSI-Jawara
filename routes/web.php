ba230fce4953d68962a8e37f33aa7646bd55870f
nadindacarissa-ctrl <nadindacarissa@student.ub.ac.id>
2025-10-28 15:02:02 +0700

    materi

--- routes/web.php
+++ routes/web.php
@@ -54,11 +54,21 @@ Route::get('/reflection-list', [ReflectionController::class, 'list'])->name('ref
 Route::get('/reflection/{id}/edit', [ReflectionController::class, 'edit'])->name('reflection.edit');
 
 
-Route::get('/admin/materi-index', [MateriController::class, 'index'])->name('materi.index');
-
 Route::get('/reflection', [ReflectionController::class, 'index'])->name('reflection');
 Route::post('/reflection', [ReflectionController::class, 'store'])->name('reflection.store');
 Route::get('/reflection-list', [ReflectionController::class, 'list'])->name('reflection.list');
 Route::get('/reflection/{id}/edit', [ReflectionController::class, 'edit'])->name('reflection.edit');
 Route::put('/reflection/{id}', [ReflectionController::class, 'update'])->name('reflection.update');
 Route::delete('/reflection/{id}', [ReflectionController::class, 'destroy'])->name('reflection.destroy');
+
+Route::get('/dashboard-admin', function () {
+    return view('dashboard.admin');
+});
+Route::get('/admin/materi-index', [MateriController::class, 'index'])->name('materi.index');
+
+Route::get('/materi', [MateriController::class, 'index'])->name('materi.index');
+Route::get('/materi/tambah', [MateriController::class, 'create'])->name('materi.create');
+Route::post('/materi/simpan', [MateriController::class, 'store'])->name('materi.store');
+Route::get('/materi/edit/{id}', [MateriController::class, 'edit'])->name('materi.edit');
+Route::post('/materi/update/{id}', [MateriController::class, 'update'])->name('materi.update');
+Route::delete('/materi/hapus/{id}', [MateriController::class, 'destroy'])->name('materi.destroy');
