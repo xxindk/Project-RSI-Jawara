ba17f63f2094162ea4505369f7c337479126709a
Hana Naila Izzah <hana.naila0306@gmail.com>
2025-10-28 09:39:24 +0700

    Save progress before pulling

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FlashcardController;
+
 
 Route::get('/', function () {
     return redirect()->route('register');
@@ -22,3 +24,12 @@ Route::get('/dashboard-user', function () {
 Route::get('/dashboard-admin', function () {
     return view('dashboard.admin');
 });
+
+Route::resource('flashcards', FlashcardController::class);
+
+Route::get('/admin/flashcards', [FlashcardController::class, 'index'])->name('flashcards.index'); 
+Route::get('/flashcard', [FlashcardController::class, 'index'])->name('flashcard.index');
+Route::post('/flashcard', [FlashcardController::class, 'store'])->name('flashcard.store');
+Route::put('/flashcard/{flashcard}', [FlashcardController::class, 'update'])->name('flashcard.update');
+Route::delete('/flashcard/{flashcard}', [FlashcardController::class, 'destroy'])->name('flashcard.destroy');
+Route::get('/flashcard-user', [FlashcardController::class, 'showForUser'])->name('flashcard.user');
