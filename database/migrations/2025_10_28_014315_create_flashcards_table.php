1e0c1977fd59c7eb91e783db266fa8a55a391c71
Hana Naila Izzah <hana.naila0306@gmail.com>
2025-11-11 07:41:43 +0700

    migration flashcard fix

--- database/migrations/2025_10_28_014315_create_flashcards_table.php
+++ database/migrations/2025_10_28_014315_create_flashcards_table.php
@@ -8,20 +8,22 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('flashcards', function (Blueprint $table) {
-            $table->id(); // primary key auto-increment
-            $table->unsignedBigInteger('id_modul'); // foreign key ke modul
-            $table->unsignedBigInteger('id_card'); // nomor flashcard dalam modul
-            $table->string('kata_indo');
-            $table->string('kata_jawa');
-            $table->timestamps();
+       Schema::create('flashcards', function (Blueprint $table) {
+    $table->id();
+    $table->unsignedBigInteger('id_modul');
+    $table->unsignedBigInteger('id_card');
+    $table->string('kata_indo');
+    $table->string('kata_jawa');
+    $table->timestamps();
 
-            // foreign key
-            $table->foreign('id_modul')->references('id')->on('moduls')->onDelete('cascade');
-            
-            // optional: supaya id_card unik per modul
-            $table->unique(['id_modul', 'id_card']);
-        });
+    $table->foreign('id_modul')
+          ->references('id_modul')
+          ->on('moduls')
+          ->onDelete('cascade');
+
+    $table->unique(['id_modul', 'id_card']);
+});
+
     }
 
     public function down(): void
